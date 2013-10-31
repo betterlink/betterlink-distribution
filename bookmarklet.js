803a566cc7adaaae373d4e134a06a72f39b48ac0
betson <ben.getson@gmail.com>
2013-10-30 20:20:36 -0400

    comment unminified version of script

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,2 +1,16 @@
 // Bookmarklet
 javascript:var betterlink;betterlink||function(a,b,c){var d,e=a.getElementsByTagName(b)[0];a.getElementById(c)||(d=a.createElement(b),d.id=c,d.defer=!0,d.src="//code.betterlink.co/betterlink.js",d.setAttribute("data-script-source","bookmarklet"),e.parentNode.insertBefore(d,e))}(document,"script","betterlink-js");
+
+/*
+var betterlink;
+if(!betterlink){
+  (function(d,s,id){
+    var js,fjs = d.getElementsByTagName(s)[0];
+    if(!d.getElementById(id)){
+    js=d.createElement(s); js.id = id; js.defer = true;
+    js.src="//code.betterlink.co/betterlink.js";
+    js.setAttribute('data-script-source','bookmarklet');
+    fjs.parentNode.insertBefore(js,fjs);}
+  }(document,'script','betterlink-js'));
+}
+*/
