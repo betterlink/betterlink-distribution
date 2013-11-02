e31fb4771773c4d946032bd619957d6e0b6efac6
betson <ben.getson@gmail.com>
2013-11-02 15:26:08 -0400

    update reference to code.betterlink.io domain

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,5 +1,5 @@
 // Bookmarklet
-javascript:var betterlink;betterlink||function(a,b,c){var d,e=a.getElementsByTagName(b)[0];a.getElementById(c)||(d=a.createElement(b),d.id=c,d.defer=!0,d.src="//code.betterlink.co/betterlink.js",d.setAttribute("data-script-source","bookmarklet"),e.parentNode.insertBefore(d,e))}(document,"script","betterlink-js");
+javascript:var betterlink;betterlink||function(a,b,c){var d,e=a.getElementsByTagName(b)[0];a.getElementById(c)||(d=a.createElement(b),d.id=c,d.defer=!0,d.src="//code.betterlink.io/betterlink.js",d.setAttribute("data-script-source","bookmarklet"),e.parentNode.insertBefore(d,e))}(document,"script","betterlink-js");
 
 /*
 var betterlink;
@@ -8,7 +8,7 @@ if(!betterlink){
     var js,fjs = d.getElementsByTagName(s)[0];
     if(!d.getElementById(id)){
     js=d.createElement(s); js.id = id; js.defer = true;
-    js.src="//code.betterlink.co/betterlink.js";
+    js.src="//code.betterlink.io/betterlink.js";
     js.setAttribute('data-script-source','bookmarklet');
     fjs.parentNode.insertBefore(js,fjs);}
   }(document,'script','betterlink-js'));
--- chrome-extension/injector.js
+++ chrome-extension/injector.js
@@ -4,7 +4,7 @@ if(!betterlink){
     var js,fjs = d.getElementsByTagName(s)[0];
     if(!d.getElementById(id)){
     js=d.createElement(s); js.id = id; js.defer = true;
-    js.src="//code.betterlink.co/betterlink.js";
+    js.src="//code.betterlink.io/betterlink.js";
     js.setAttribute('data-script-source','chrome extension');
     fjs.parentNode.insertBefore(js,fjs);}
   }(document,'script','betterlink-js'));
