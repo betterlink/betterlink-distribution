4e56921a60b2c8469af931200849a286b13ab42f
betson <ben.getson@gmail.com>
2013-11-05 00:05:40 -0500

    prevent injection on betterlink.io homepage
    At the moment, there's a problem if we load multiple versions of
    *different* Betterlink instances on the same page. Because the
    betterlink.io homepage uses betterlink-bespoke.js to customize the
    experience, injecting the vanilla betterlink.js before the customized
    version runs may be unwanted. For now, this prevents that experience
    specifically.

--- chrome-extension/injector.js
+++ chrome-extension/injector.js
@@ -2,11 +2,12 @@ var betterlink;
 if(!betterlink){
   (function(d,s,id){
     var js,scripts = d.getElementsByTagName(s),fjs = scripts[0];
-    if(!d.getElementById(id) && !scriptsInDomWithBetterlinkSource(scripts)){
-    js=d.createElement(s); js.id = id; js.defer = true;
-    js.setAttribute('data-script-source','chrome extension');
-    js.src="//code.betterlink.io/betterlink.js";
-    fjs.parentNode.insertBefore(js,fjs);}
+    if(!d.getElementById(id) && !scriptsInDomWithBetterlinkSource(scripts)
+          && !onBetterlinkHomepage()) {
+      js=d.createElement(s); js.id = id; js.defer = true;
+      js.setAttribute('data-script-source','chrome extension');
+      js.src="//code.betterlink.io/betterlink.js";
+      fjs.parentNode.insertBefore(js,fjs);}
   }(document,'script','betterlink-js'));
 
   // Chrome Extensions run in an 'isolated world' and cannot
@@ -26,5 +27,22 @@ if(!betterlink){
       }
     }
     return foundScripts;
+  }
+
+  // At the moment, there's a problem if we load multiple versions of
+  // *different* Betterlink instances on the same page. Because the
+  // betterlink.io homepage uses betterlink-bespoke.js to customize
+  // the experience, injecting the vanilla betterlink.js before the
+  // customized version runs may be unwanted. For now, this prevents
+  // that experience specifically.
+  // Using exclude_matches in the manifest would also work here, but
+  // we've got added flexibility with the TLD this way.
+  function onBetterlinkHomepage() {
+    // Matches a betterlink host (ex: betterlink.io), but fails
+    // if `betterlink` is provided as a third-level domain or
+    // higher (ex: betterlink.other-site.com). Also fails for
+    // any subdomains of betterlink (ex: code.betterlink.io).
+    var onHomepage = /^betterlink\.[^\.]+(?!.*\.)/;
+    return onHomepage.test(window.location.host);
   }
 }
