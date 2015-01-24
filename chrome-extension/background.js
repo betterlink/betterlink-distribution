067fd6a31945ab069806d72f1a9e640115dcc9cc
betson <ben@betterlink.io>
2015-01-24 10:23:43 -0500

    if style-src CSP directive, supply nonce to script
    If the client website has defined a Content Security Policy that include
    'style-src', then we need to apply a nonce to the Betterlink script --
    which will apply the nonce to each style element that is created by the
    script.
    
    The nonce is a signal to the browser that the style element is allowed.
    If a nonce is already supplied by the server, then we should use that
    nonce. If one is not provided, then we should create it ourselves.

--- chrome-extension/background.js
+++ chrome-extension/background.js
@@ -6,6 +6,10 @@
  * Derived from:
  * https://www.planbox.com/blog/development/coding/bypassing-githubs-content-security-policy-chrome-extension.html
  */
+var styleSrcRegex = /\bstyle-src\b/;
+var nonceRegex = /style-src[^;]*'nonce-([^']*)'/;
+var nonceValues = {};
+
 chrome.webRequest.onHeadersReceived.addListener(function(details) {
   for (i = 0; i < details.responseHeaders.length; i++) {
 
@@ -15,6 +19,7 @@ chrome.webRequest.onHeadersReceived.addListener(function(details) {
       // append "//code.betterlink.io" to the authorized sites
       csp = csp.replace('script-src', 'script-src https://code.betterlink.io http://code.betterlink.io');
       csp = csp.replace('style-src', 'style-src https://code.betterlink.io http://code.betterlink.io');
+      csp = handleStyleSrcNonce(csp, details.url);
 
       details.responseHeaders[i].value = csp;
     }
@@ -26,7 +31,46 @@ chrome.webRequest.onHeadersReceived.addListener(function(details) {
   types: ["main_frame"]
 }, ["blocking", "responseHeaders"]);
 
+// Respond to messages from Content Scripts asking if a
+// nonce was provided in the Content Security Policy
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    if(request.checkNonce) {
+      sendResponse({nonce: nonceValues[sender.tab.url]});
+    }
+  }
+);
 
 function isCSPHeader(headerName) {
   return (headerName == 'CONTENT-SECURITY-POLICY') || (headerName == 'X-WEBKIT-CSP');
+}
+
+// If the 'style-src' directive is supplied, then check if a nonce
+// is provided as well. If so, use it. If not, create one.
+//
+// This is needed to allow us to create inline styles via Betterlink.
+// http://www.w3.org/TR/CSP2/#script-src-nonce-usage
+function handleStyleSrcNonce(csp, url) {
+  if(styleSrcRegex.test(csp)) {
+    var match = nonceRegex.exec(csp);
+    if(match) {
+      nonceValues[url] = match[1];
+    }
+    else {
+      var random = generateRandom();
+      csp = csp.replace("style-src", "style-src 'nonce-" + random + "'");
+      nonceValues[url] = random;
+    }
+  }
+
+  return csp;
+}
+
+// Randomly generates a unique identifier
+function generateRandom() {
+  var out = '';
+  for(var i = 0, count = 8; i < count; i++) {
+    out += (((1+Math.random())*0x10000)|0).toString(16).substring(1);
+  }
+  return out;
 }
--- chrome-extension/injector.js
+++ chrome-extension/injector.js
@@ -1,33 +1,40 @@
 var betterlink;
-if(!betterlink){
-  (function(d,s,id){
-    // We can't inject relative to the first script on the page because
-    // there may not be any. This javascript is run in an isolated world.
-    var scripts = d.getElementsByTagName(s);
-    var node = d.getElementsByTagName('head')[0] || d.body;
-    if(!d.getElementById(id) && !scriptsInDomWithBetterlinkSource(scripts)) {
-      js=d.createElement(s); js.id = id; js.defer = true;
-      js.setAttribute('data-script-source','chrome extension');
-      js.src="//code.betterlink.io/betterlink.js";
-      node.appendChild(js);}
-  }(document,'script','betterlink-js'));
+chrome.runtime.sendMessage({checkNonce: true}, function(response) {
+  addBetterlinkScript(response.nonce);
+});
 
-  // Chrome Extensions run in an 'isolated world' and cannot
-  // interact with JavaScript variables or functions created
-  // by the page.
-  // http://developer.chrome.com/extensions/content_scripts.html
-  // Instead, we can check against the source files for Betterlink
-  function scriptsInDomWithBetterlinkSource(scripts) {
-    var foundScripts = false;
-    // match against a Betterlink script (ex: /betterlink-bespoke.js),
-    // excluding poorly-named scripts on the Betterlink homepage
-    var scriptInSource = /\/betterlink(?!-site)[^\/]*\.js/;
-    for(var i = 0, len = scripts.length; i < len; i++) {
-      if(scriptInSource.test(scripts[i].src)) {
-        foundScripts = true;
-        break;
-      }
-    }
-    return foundScripts;
+function addBetterlinkScript(nonce) {
+  if(!betterlink){
+    (function(d,s,id){
+      // We can't inject relative to the first script on the page because
+      // there may not be any. This javascript is run in an isolated world.
+      var scripts = d.getElementsByTagName(s);
+      var node = d.getElementsByTagName('head')[0] || d.body;
+      if(!d.getElementById(id) && !scriptsInDomWithBetterlinkSource(scripts)) {
+        js=d.createElement(s); js.id = id; js.defer = true;
+        js.setAttribute('data-script-source','chrome extension');
+        if(nonce) { js.setAttribute('data-nonce', nonce); }
+        js.src="//code.betterlink.io/betterlink.js";
+        node.appendChild(js);}
+    }(document,'script','betterlink-js'));
   }
+}
+
+// Chrome Extensions run in an 'isolated world' and cannot
+// interact with JavaScript variables or functions created
+// by the page.
+// http://developer.chrome.com/extensions/content_scripts.html
+// Instead, we can check against the source files for Betterlink
+function scriptsInDomWithBetterlinkSource(scripts) {
+  var foundScripts = false;
+  // match against a Betterlink script (ex: /betterlink-bespoke.js),
+  // excluding poorly-named scripts on the Betterlink homepage
+  var scriptInSource = /\/betterlink(?!-site)[^\/]*\.js/;
+  for(var i = 0, len = scripts.length; i < len; i++) {
+    if(scriptInSource.test(scripts[i].src)) {
+      foundScripts = true;
+      break;
+    }
+  }
+  return foundScripts;
 }
